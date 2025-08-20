9d6eacba6604c3caedb0a0c570c1b867579d49be
pinyili2 <pinyili2@illinois.edu>
2025-08-19 22:42:37 -0500

    add device mesh

--- src/Backend/Collective.h
+++ src/Backend/Collective.h
@@ -106,4 +106,72 @@ class Collectives {
 	}
 };
 
+class DeviceMesh {
+  private:
+	std::vector<Resource> resources_;
+	std::unordered_map<int, void*> queues_; // device_id -> queue
+	bool peer_access_enabled_ = false;
+
+  public:
+	DeviceMesh() = default;
+
+	explicit DeviceMesh(std::vector<Resource> resources) : resources_(resources) {
+		for (const auto& res : resources_) {
+#ifdef USE_CUDA
+			if (res.type == ResourceType::CUDA) {
+				cudaStream_t queue;
+				CUDA_CHECK(cudaSetDevice(res.id));
+				CUDA_CHECK(cudaStreamCreate(&queue));
+				queues_[res.id] = queue;
+			}
+#endif
+
+#ifdef USE_SYCL
+			if (res.type == ResourceType::SYCL) {
+				auto& device = SYCL::Manager::get_device(res.id);
+				queues_[res.id] = &device.get_queue(0);
+			}
+#endif
+		}
+
+		enable_peer_access();
+	}
+
+	~DeviceMesh() {
+#ifdef USE_CUDA
+		for (const auto& res : resources_) {
+			if (res.type == ResourceType::CUDA) {
+				cudaSetDevice(res.id);
+				cudaStreamDestroy(static_cast<cudaStream_t>(queues_[res.id]));
+			}
+		}
+#endif
+	}
+
+	void enable_peer_access() {
+#ifdef USE_CUDA
+		for (size_t i = 0; i < resources_.size(); ++i) {
+			if (resources_[i].type != ResourceType::CUDA)
+				continue;
+
+			cudaSetDevice(resources_[i].id);
+			for (size_t j = 0; j < resources_.size(); ++j) {
+				if (i != j && resources_[j].type == ResourceType::CUDA) {
+					int can_access;
+					cudaDeviceCanAccessPeer(&can_access, resources_[i].id, resources_[j].id);
+					if (can_access) {
+						cudaDeviceEnablePeerAccess(resources_[j].id, 0);
+					}
+				}
+			}
+		}
+#endif
+		peer_access_enabled_ = true;
+	}
+
+	void* get_queue(int device_id) {
+		auto it = queues_.find(device_id);
+		return (it != queues_.end()) ? it->second : nullptr;
+	}
+};
 } // namespace ARBD
